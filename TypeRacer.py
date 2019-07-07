43026567f53fe1a3b6e61c0f80a1e921f82baeaa
edelosre <36717398+edelosre@users.noreply.github.com>
2019-07-07 01:44:22 -0500

    Add files via upload
    Cleaned variable names

--- TypeRacer.py
+++ TypeRacer.py
@@ -5,9 +5,8 @@ import random
 from selenium import webdriver
 
 class TypeRacerPublicGame(webdriver.Chrome):
-    #Initialize selenium script
     def __init__(self, element_wait = 30):
-        super().__init__()
+        super().__init__() #Inherit methods from webdriver.Chrome class
         self.get('https://play.typeracer.com/')
         self.implicitly_wait(element_wait)
 
@@ -15,6 +14,7 @@ class TypeRacerPublicGame(webdriver.Chrome):
         self.find_element_by_xpath("//a[contains(text(),'Enter a typing race')]").click()
     
     def get_typing_string(self):
+        #Absolute paths are used since CSS id's are randomized
         first_word_path = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[1]'
         first_word__path_part2 = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[2]'
         following_string_path = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[3]'
@@ -27,14 +27,14 @@ class TypeRacerPublicGame(webdriver.Chrome):
         return(full_string)
     
     def start_typing(self, wait_interval_begin = 0.05, wait_interval_end = 0.20):
-        write_box = self.find_element_by_xpath("//input[@class='txtInput']")
+        typing_box = self.find_element_by_xpath("//input[@class='txtInput']")
         random_wait = random.uniform(wait_interval_begin, wait_interval_end)
         full_string = self.get_typing_string()
         for string in full_string.split():
             for letter in string:
                 time.sleep(random_wait)
-                write_box.send_keys(letter)
-            write_box.send_keys(' ')
+                typing_box.send_keys(letter)
+            typing_box.send_keys(' ')
             
 def main():
     public_game = TypeRacerPublicGame()
