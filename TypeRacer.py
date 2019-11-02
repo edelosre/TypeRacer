13dd99e75c89b4bf85ea33fc91d4a85f01b74922
edelosre <36717398+edelosre@users.noreply.github.com>
2019-11-01 21:54:54 -0700

    Updated element paths and added gif

--- TypeRacer.py
+++ TypeRacer.py
@@ -2,22 +2,37 @@
 
 import time
 import random
+import os
 from selenium import webdriver
 
 class TypeRacerPublicGame(webdriver.Chrome):
-    def __init__(self, element_wait = 30):
+    def __init__(self, private_game_url = False, element_wait = 30):
         super().__init__() #Inherit methods from webdriver.Chrome class
-        self.get('https://play.typeracer.com/')
+        if private_game_url == False:
+            self.get('https://play.typeracer.com/')
+            self.private_game = False
+        else:
+            self.private_game = True
+            self.get(private_game_url)
         self.implicitly_wait(element_wait)
 
     def click_start_race(self):
-        self.find_element_by_xpath("//a[contains(text(),'Enter a typing race')]").click()
+        if self.private_game == False:
+            self.find_element_by_xpath("//a[contains(text(),'Enter a typing race')]").click()
+        else:
+            self.find_element_by_xpath("//a[@class='raceAgainLink']").click()
     
     def get_typing_string(self):
-        #Absolute paths are used since CSS id's are randomized
-        first_word_path = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[1]'
-        first_word__path_part2 = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[2]'
-        following_string_path = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[3]'
+        #Absolute paths are used since CSS classes and element id are randomzied
+        if self.private_game == False:
+            first_word_path = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[1]'
+            first_word__path_part2 = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[2]'
+            following_string_path = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[3]'
+        else:
+            first_word_path =  '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[1]'
+            first_word__path_part2 = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[2]'
+            following_string_path = '/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[2]/div[1]/div[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]/div[1]/table[1]/tbody[1]/tr[2]/td[3]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/table[1]/tbody[1]/tr[1]/td[1]/div[1]/div[1]/span[3]'
+            
         
         first_word = self.find_element_by_xpath(first_word_path).text
         first_word = first_word + self.find_element_by_xpath(first_word__path_part2).text
@@ -37,10 +52,13 @@ class TypeRacerPublicGame(webdriver.Chrome):
             typing_box.send_keys(' ')
             
 def main():
+    os.chdir("C:/Users/Josiah/Documents/Song-Lyric-Script")
     public_game = TypeRacerPublicGame()
     public_game.click_start_race()
     public_game.get_typing_string()
-    public_game.start_typing(wait_interval_begin = 0.01, wait_interval_end = 0.05)
+    public_game.start_typing(wait_interval_begin = 0.01, wait_interval_end = 0.06)
     
 if __name__ == '__main__':
     main()
+   
+    
